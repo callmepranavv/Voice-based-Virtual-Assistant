a5e13647056a79f086599966864f1e728c91cee8
Pranav Shrivastava <63885865+callmepranavv@users.noreply.github.com>
2021-07-24 02:29:06 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+# get audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak:")
+    audio = r.listen(source)
+
+try:
+    print("You said " + r.recognize_google(audio))
+except:
+    pass
